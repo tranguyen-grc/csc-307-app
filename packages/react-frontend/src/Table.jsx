5f2a7d26817e3a52729a558a60e013c4f1e6ff77
tranguyen-grc <chiptra292@gmail.com>
2025-04-03 14:17:21 -0700

    created the table (up until step 5)

---
+++ packages/react-frontend/src/Table.jsx
@@ -0,0 +1,43 @@
+// src/Table.jsx
+import React from "react";
+
+function TableHeader() {
+  return (
+    <thread>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Edit</th>
+      </tr>
+    </thread>
+  );
+}
+
+function TableBody(props) {
+  const rows = props.characterData.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
+      </tr>
+    );
+  });
+  return <tbody>{rows}</tbody>;
+}
+
+function Table(props) {
+  return (
+    <table>
+      <TableHeader />
+      <TableBody
+        characterData={props.characterData}
+        removeCharacter={props.removeCharacter}
+      />
+    </table>
+  );
+}
+
+export default Table;
